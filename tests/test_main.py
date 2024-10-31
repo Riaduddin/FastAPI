39b07e6d45d05f6748f2261724ae8e7b45bedd25
Kazi Riad Uddin <rrriaduddin@gmail.com>
2024-10-31 11:11:15 +0600

    changing test_main file and install_dependencies.hs

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
+import sys
+import os
 from fastapi.testclient import TestClient
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app.main import app
 
 client = TestClient(app)
@@ -7,3 +10,4 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World!"}
+test_read_root()
